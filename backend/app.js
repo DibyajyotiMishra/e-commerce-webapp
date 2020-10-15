7bae855ccfa27a6494df5113a1a90e8e0a097264
Balaji Kotni <67455293+Balaji-Kotni@users.noreply.github.com>
2020-10-15 17:56:59 +0530

    Added Payment  Gateway

--- backend/app.js
+++ backend/app.js
@@ -39,6 +39,39 @@ app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
 app.use("/api", paymentBRoutes);
 
+//Added Payment Backend.
+exports.createOrder = async (req, res) => {
+  const amount = req.doctor.fees * 100;
+
+  const payment_capture = 1;
+  const currency = "INR";
+
+  const options = {
+    amount: amount,
+    currency,
+    receipt: req.doctor._id.toString(),
+    payment_capture,
+  };
+  try {
+    const response = await razorpay.orders.create(options, (err, order) => {
+      if (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ error: "Some Error has occurred. Are you online?" });
+      }
+
+      return res.status(200).json({
+        id: order.id,
+        amount: order.amount,
+        currency: order.currency,
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 //PORT
 const port = process.env.PORT || 8000;
 
